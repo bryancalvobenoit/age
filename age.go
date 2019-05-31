8f9d8a2db107467741e782db60c3301329b03ef4
Bryan Calvo <bryancalvobenoit@gmail.com>
2019-05-31 00:26:25 -0400

    Initial Commit

---
+++ age.go
@@ -0,0 +1,32 @@
+package age
+
+import (
+	"time"
+)
+
+type EntryDate struct {
+	day   int
+	month int
+	year  int
+}
+
+type CurrentDate struct {
+	day   int
+	month int
+	year  int
+}
+
+func getCurrentDate() *CurrentDate {
+	date := new(CurrentDate)
+	t := time.Now()
+	date.day = t.Day()
+	date.month = int(t.Month())
+	date.year = t.Year()
+
+	return date
+}
+
+func getAge(date EntryDate) int {
+
+	return date.year
+}
